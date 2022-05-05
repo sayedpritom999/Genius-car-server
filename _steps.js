53c1dcce533f63a2ac0c8b5337fc8974d94fce7a
unknown <sayedpritom999@gmail.com>
2022-05-06 00:18:46 +0600

    adding steps

--- _steps.js
+++ _steps.js
@@ -16,5 +16,29 @@ require('dotenv').config()
 13. replace them in uri ${process.env.DB_USER}:${process.env.DB_PASS}
 14. follow https://www.mongodb.com/docs/drivers/node/current/usage-examples/find/
 15. To get single data: require ObjectId from mongodb // const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-16.  
+16. save orders for multiple user and save one account per user
+17. filter orders info by email address
+18. Install, create jwt token(create using node), 
+
  */
+
+
+/**
+ * Module 66_5 PRACTICE(MONGO DB CRUD WITH NODE, EXPRESS, REACT)
+1. Create node.js a server with express(five steps)
+2. connect to database
+3. Create a post API to send data. Use cors & express.json as middleware here
+4. create a simple react app with route: addProduct(react hook form. Name, price, quantity inputs). onSubmit saved data from server to database. C done
+5. create manageProducts route. Create api to Load all data & load. 
+6. add delete button, update button to each product. create delete api in server
+7. create update route & link it with update button. 
+8. use dynamic id. useParams. load data in a form in the update route using that id.
+9. create API for Update product. onSubmit update the product. U done 
+ */
+
+
+// heroku (server deploy)
+// install heroku cli 
+// make sure "const port = process.env.PORT || 5000;" & ""start": "node index.js"," is setup 
+// heroku login 
+// 
--- index.js
+++ index.js
@@ -3,20 +3,59 @@ const cors = require('cors');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const port = process.env.PORT || 5000;
+var jwt = require('jsonwebtoken');
+var token = jwt.sign({ foo: 'bar' }, 'shhhhh');
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
+function verifyJWT (req, res, next) {
+    const authHeader = req.headers.authorization;
+    if(!authHeader) {
+        return res.status(401).send({message: 'unauthorized access'})
+    }
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if(err) {
+            return res.status(403).send({message: 'forbidden message'})
+        }
+        console.log('decoded', decoded)
+        req.decoded = decoded;
+        next();
+    })
+}
 
-
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.osdnk.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.wurf8.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
 async function run() {
     try{
         await client.connect();
         const serviceCollection = client.db('geniusCar').collection('service');
+        const orderCollection = client.db('geniusCar').collection('order');
 
+        client.connect(err => {
+            const collection = client.db("test").collection("devices");
+            // perform actions on the collection object
+            // client.close();
+            console.log("connected")
+          });
+
+        // Auth
+        app.post('/login', (req, res) => {
+            const user = req.body;
+            console.log(user)
+            const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+                expiresIn: '1d' 
+            })
+            res.send({accessToken})
+        })
+
+
+
+
+        // services API
         app.get('/service', async (req, res) => {
             const query = {};
             const cursor = serviceCollection.find(query);
@@ -43,6 +82,27 @@ async function run() {
             const query = {_id: ObjectId(id)}
             const result = await serviceCollection.deleteOne(query);
             res.send(result);
+        })
+
+        // order collection api
+
+        app.get('/order', verifyJWT,  async (req, res) => {
+            const decodedEmail = req.decoded.email;
+            const email = req.query.email;
+            if(email === decodedEmail) {
+                const query = {email: email};
+                const cursor = await orderCollection.find(query);
+                const orders = await cursor.toArray();
+                res.send(orders);
+            } else {
+                res.send(403).send({message: 'Forbidden access'})
+            }
+        })
+        
+        app.post('/order', async (req, res) => {
+            const order = req.body;
+            const result = await orderCollection.insertOne(order);
+            res.send(result); 
         })
 
     }
